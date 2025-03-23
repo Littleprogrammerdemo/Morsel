346ecdc13af1716ab4fab423172279e234f523f2
suleimanapaspalova <157298073+suleimanapaspalova@users.noreply.github.com>
2025-03-23 09:25:22 -0700

    Error handling - second fix

--- app/src/main/java/app/web/ExceptionAdvice.java
+++ app/src/main/java/app/web/ExceptionAdvice.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
 public class ExceptionAdvice {
@@ -36,8 +37,10 @@ public class ExceptionAdvice {
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(
-            MissingRequestValueException.class //When a required request parameter is missing.
+    @ExceptionHandler({
+            MissingRequestValueException.class,//When a required request parameter is missing.
+            NoResourceFoundException.class
+    }
     )
     public ModelAndView handleNotFoundExceptions(Exception exception) {
 
